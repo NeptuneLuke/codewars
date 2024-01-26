3c98bc15bcf7abbed7a34b93ad56181b4c1cf607
NeptuneLuke <luca1cortinovis@gmail.com>
2024-01-26 13:03:33 +0100

    first commit

---
+++ 6_kyu/multiplies_of_3_or_5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+
+
+using 
+std::cout,
+std::cin;
+
+
+int solution(int number) {
+    
+    if(number < 0) {
+        return 0;
+    }
+    
+    int sum = 0;
+    for(int i=0; i < number; i++) {
+        
+        if (i % 3 == 0 || i % 5 == 0) {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
+
+
+int main(void) {
+
+    assert(solution(10) == 23 && "Success");
+    assert(solution(20) == 78 && "Success");
+    assert(solution(200) == 9168 && "Success");
+
+    assert(solution(-1) == 0 && "Success");
+    assert(solution(0) == 0 && "Success");
+    assert(solution(1) == 0 && "Success");
+    assert(solution(2) == 0 && "Success");
+    assert(solution(3) == 0 && "Success");
+    assert(solution(4) == 3 && "Success");
+    assert(solution(5) == 3 && "Success");
+    assert(solution(6) == 8 && "Success");
+
+    return 0;
+}
