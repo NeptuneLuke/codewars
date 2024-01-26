4b451cd24a46ce4f84eec7fb25accee77c12e65e
NeptuneLuke <luca1cortinovis@gmail.com>
2024-01-26 13:47:42 +0100

    Format fix 6_kyu/multiplies_of_3_or_5.cpp

--- 6_kyu/multiplies_of_3_or_5.cpp
+++ 6_kyu/multiplies_of_3_or_5.cpp
@@ -4,11 +4,6 @@
 #include <cassert>
 
 
-using 
-std::cout,
-std::cin;
-
-
 int solution(int number) {
     
     if(number < 0) {
@@ -29,18 +24,19 @@ int solution(int number) {
 
 int main(void) {
 
-    assert(solution(10) == 23 && "Success");
-    assert(solution(20) == 78 && "Success");
-    assert(solution(200) == 9168 && "Success");
+    assert(solution(10) == 23);
+    assert(solution(20) == 78);
+    assert(solution(200) == 9168);
 
-    assert(solution(-1) == 0 && "Success");
-    assert(solution(0) == 0 && "Success");
-    assert(solution(1) == 0 && "Success");
-    assert(solution(2) == 0 && "Success");
-    assert(solution(3) == 0 && "Success");
-    assert(solution(4) == 3 && "Success");
-    assert(solution(5) == 3 && "Success");
-    assert(solution(6) == 8 && "Success");
+    assert(solution(-1) == 0);
+    assert(solution(0) == 0);
+    assert(solution(1) == 0);
+    assert(solution(2) == 0);
+    assert(solution(3) == 0);
+    assert(solution(4) == 3);
+    assert(solution(5) == 3);
+    assert(solution(6) == 8);
+
 
     return 0;
 }
